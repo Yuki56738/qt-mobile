8be7c3645899be8f3b97a88f653f3be89ee2616d
Yuki <github@risaton.net>
2024-02-24 22:13:19 +0900

    commit

--- build-qt-mobile-Replacement_for_Qt_6_6_2_for_iOS_496cff/build/qt-mobile.build/Debug/Objects-normal/arm64/main.d
+++
@@ -1,2 +0,0 @@
-dependencies: /Users/yuki/CloneHub/qt-mobile/qt-mobile/main.cpp \
-  /Users/yuki/CloneHub/qt-mobile/qt-mobile/mainwindow.h
--- build-qt-mobile-Replacement_for_Qt_6_6_2_for_iOS_496cff/build/qt-mobile.build/Debug/Objects-normal/arm64/mocs_compilation.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/yuki/CloneHub/qt-mobile/build-qt-mobile-Replacement_for_Qt_6_6_2_for_iOS_496cff/qt-mobile_autogen/mocs_compilation.cpp \
-  /Users/yuki/CloneHub/qt-mobile/build-qt-mobile-Replacement_for_Qt_6_6_2_for_iOS_496cff/qt-mobile_autogen/include_Debug/EWIEGA46WW/moc_mainwindow.cpp \
-  /Users/yuki/CloneHub/qt-mobile/build-qt-mobile-Replacement_for_Qt_6_6_2_for_iOS_496cff/qt-mobile_autogen/include_Debug/EWIEGA46WW/../../../../qt-mobile/mainwindow.h
